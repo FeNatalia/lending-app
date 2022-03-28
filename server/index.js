30ceb85f425a8f5badd17c2622465e12b5ad03dd
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-03-28 13:24:21 +0200

    edit server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-// const path = require("path")
+const path = require("path")
 const cors = require("cors");
 
 const app = express();
@@ -9,13 +9,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-// app.use(express.static(path.join(__dirname,"../client/build")));
-// app.use('/',(req, res) => res.sendFile(path.join(__dirname,"../client/build/index.html")));
-
+app.use(express.static(path.join(__dirname,"../client/build")));
 
 app.get("/api", (req, res) => {
   return res.json({ message: "You have reached the Lender API" });
 });
+
+app.use((req, res) => res.sendFile(path.join(__dirname,"../client/build/index.html")));
 
 if (!module.parent) {
   app.listen(port);
