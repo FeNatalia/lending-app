e7e8670fdf97a064652c058868aadb4862fa73f2
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-03-28 10:06:53 +0200

    add dynamic port

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+
+// const path = require("path")
 const cors = require("cors");
 
 const app = express();
 
-const port = 8080;
+
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
+// app.use(express.static(path.join(__dirname,"../client/build")));
+// app.use('/',(req, res) => res.sendFile(path.join(__dirname,"../client/build/index.html")));
+
 
 app.get("/api", (req, res) => {
   return res.json({ message: "You have reached the Lender API" });
