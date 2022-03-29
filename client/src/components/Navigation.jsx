c06d35233cdc6f220eefae6d4106f11ff1c7ef6f
Leila Mahmoudi <leila.mahmoudi9169@gmail.com>
2022-03-29 13:32:31 +0200

    add feed to nav, add image input in addnew form

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,32 +1,27 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">
-          Logo
-        </Link>
-      </li>
-      <li>
-        <Link to="/about">
-          About
-        </Link>
-      </li>
-      <li>
-        <Link to="/add-item">
-          Add New Item
-        </Link>
-      </li>
-      <li>
-        <Link to="/login">
-          Login
-        </Link>
-      </li>
-   </ul>
-  </nav>
-  )
-}
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Logo</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/feed">Feed</Link>
+        </li>
+        <li>
+          <Link to="/add-item">Add New Item</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
