bea2c3aeda2655d6ccddfbe3ae1d59e605e72e10
Leila Mahmoudi <leila.mahmoudi9169@gmail.com>
2022-04-04 23:24:33 +0200

    fixed search bar bug

--- client/src/contexts/DataProvider.jsx
+++ client/src/contexts/DataProvider.jsx
@@ -2,10 +2,20 @@ import React, { createContext, useState } from "react";
 
 export const DataContext = createContext();
 export const DataProvider = ({ children }) => {
+  const [items, setItems] = useState([]);
   const [city, setCity] = useState("");
   const [category, setCategory] = useState();
   const [keyword, setKeyword] = useState("");
-  const value = { category, setCategory, city, setCity, keyword, setKeyword };
+  const value = {
+    category,
+    setCategory,
+    city,
+    setCity,
+    keyword,
+    setKeyword,
+    items,
+    setItems,
+  };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
