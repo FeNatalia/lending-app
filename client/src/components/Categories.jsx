ab20694d91ebaa949b6a310d92d642c9161db343
Leila Mahmoudi <leila.mahmoudi9169@gmail.com>
2022-04-05 14:31:24 +0200

    Add styles to category section

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -7,27 +7,27 @@ const Categories = () => {
     {
       name: "Electronics",
       image:
-        "https://academy.fredsappliance.com/wp-content/uploads/2020/11/tools1.jpg",
+        "https://thumbs.dreamstime.com/b/set-yellow-power-tools-professional-craftsman-vector-illustration-instruments-handyman-flat-style-renovation-181761505.jpg",
     },
     {
       name: "Tools",
       image:
-        "https://academy.fredsappliance.com/wp-content/uploads/2020/11/tools1.jpg",
+        "https://image.shutterstock.com/image-vector/construction-tools-set-vector-icons-260nw-1189237477.jpg",
     },
     {
       name: "Camping",
       image:
-        "https://academy.fredsappliance.com/wp-content/uploads/2020/11/tools1.jpg",
+        "https://cdn.w600.comps.canstockphoto.com/camping-supplies-tools-and-equipment-vector-clip-art_csp74593118.jpg",
     },
     {
       name: "Garden",
       image:
-        "https://academy.fredsappliance.com/wp-content/uploads/2020/11/tools1.jpg",
+        "https://previews.123rf.com/images/dreamsvector/dreamsvector1801/dreamsvector180100017/93570036-gartenwerkzeugvektorgartenger%C3%A4t-r%C3%BChrstange-oder-schaufel-und-rasenm%C3%A4her-der-g%C3%A4rtnerbauernhofsammlung.jpg",
     },
     {
       name: "Renovation",
       image:
-        "https://academy.fredsappliance.com/wp-content/uploads/2020/11/tools1.jpg",
+        "https://images.clipartlogo.com/files/istock/previews/9843/98439719-repair-and-renovation-tools-hand-drawn-vector-icons.jpg",
     },
   ];
   return (
