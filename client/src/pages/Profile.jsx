8e3c3124812f379c05a6da3d10330c5cd4751a29
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-04-06 09:26:34 +0200

    edit profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,186 +17,105 @@ export const Profile = () => {
   useEffect(() => fetchUser(), [fetchUser]);
 
   return (
-    <div className="flex">
+    <div id="profile-page-wrapper">
       <SideBar />
       {isLogged && (
-        <header>
-          <h1>User Details:</h1>
-          <h3>Name: {user.name}</h3>
-          <p>E-mail: {user.email}</p>
-        </header>
-        /*<div className="mt-10 sm:mt-0">
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <form action="#" method="POST">
-                <div className="shadow overflow-hidden sm:rounded-md">
-                  <div className="px-4 py-5 bg-white sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="first-name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          First name
-                        </label>
-                        <input
-                          type="text"
-                          value={user.name}
-                          name="first-name"
-                          id="first-name"
-                          autoComplete="given-name"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="last-name"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Last name
-                        </label>
-                        <input
-                          type="text"
-                          name="last-name"
-                          id="last-name"
-                          autoComplete="family-name"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-4">
-                        <label
-                          htmlFor="email-address"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email address
-                        </label>
-                        <input
-                          type="text"
-                          value={user.email}
-                          name="email-address"
-                          id="email-address"
-                          autoComplete="email"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="flex flex-col">
-                        <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                          <svg
-                            className="h-full w-full text-gray-300"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                          </svg>
-                        </span>
-                        <label className="block text-sm font-medium text-gray-700">
+        // <header>
+        //   <h1>User Details:</h1>
+        //   <h3>Name: {user.name}</h3>
+        //   <p>E-mail: {user.email}</p>
+        // </header>
+        <div id="profile-page">
+          <section className="profile-page___form">
+            <div className="mt-10 sm:mt-0">
+              <div className="md:grid md:grid-cols-3 md:gap-6">
+                <div className="mt-5 md:mt-0 md:col-span-2">
+                  <form action="#" method="POST">
+                    <div className="shadow overflow-hidden sm:rounded-md">
+                      <div className="px-4 py-5 bg-white sm:p-6">
+                        <div className="grid grid-cols-6 gap-6">
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="first-name"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Full name
+                            </label>
+                            <input
+                              type="text"
+                              value={user.name}
+                              name="first-name"
+                              id="first-name"
+                              autoComplete="given-name"
+                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            />
+                          </div>
+                          <div className="flex flex-col">
+                            <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                              <svg
+                                className="h-full w-full text-gray-300"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                              >
+                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                              </svg>
+                            </span>
+                            {/* <label className="block text-sm font-medium text-gray-700">
                           Photo
                         </label>
                         <div className="mt-1 flex-col sm:flex-1">
                           <input type="file" className="rounded-md" />
+                        </div> */}
+                          </div>
+                          <div className="col-span-6 sm:col-span-4">
+                            <label
+                              htmlFor="email-address"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Email address
+                            </label>
+                            <input
+                              type="text"
+                              value={user.email}
+                              name="email-address"
+                              id="email-address"
+                              autoComplete="email"
+                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                            />
+                          </div>
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="country"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Location
+                            </label>
+                            <select
+                              id="country"
+                              name="country"
+                              autoComplete="country-name"
+                              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            >
+                              <option>Sweden</option>
+                            </select>
+                          </div>
                         </div>
                       </div>
-
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="country"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Location
-                        </label>
-                        <select
-                          id="country"
-                          name="country"
-                          autoComplete="country-name"
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        >
-                          <option>Sweden</option>
-                          <option>Stockholm</option>
-                          <option>Mexico</option>
-                        </select>
-                      </div>
-
-                      <div className="col-span-6">
-                        <label
-                          htmlFor="street-address"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Street address
-                        </label>
-                        <input
-                          type="text"
-                          name="street-address"
-                          id="street-address"
-                          autoComplete="street-address"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                        <label
-                          htmlFor="city"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          City
-                        </label>
-                        <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          autoComplete="address-level2"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                        <label
-                          htmlFor="region"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          State / Province
-                        </label>
-                        <input
-                          type="text"
-                          name="region"
-                          id="region"
-                          autoComplete="address-level1"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                        <label
-                          htmlFor="postal-code"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          ZIP / Postal code
-                        </label>
-                        <input
-                          type="text"
-                          name="postal-code"
-                          id="postal-code"
-                          autoComplete="postal-code"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                      {/* <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
                       type="submit"
                       className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Save
                     </button>
-                  </div>
+                  </div> */}
+                    </div>
+                  </form>
                 </div>
-              </form>
+              </div>
             </div>
-          </div>
-        </div>*/
+          </section>
+        </div>
       )}
     </div>
   );
