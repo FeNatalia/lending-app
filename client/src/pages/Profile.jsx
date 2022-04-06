e7006d9ff03314d6830cfd874a93cf6f96bfdf3c
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-04-06 11:32:02 +0200

    solve pull problems

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -96,7 +96,7 @@ export const Profile = () => {
                                 <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                               </svg>
                             </span>
-                            <div className="grid-cols-2 mt-2">
+                            {/* <div className="grid-cols-2 mt-2">
                               <label
                                 htmlFor="file-upload"
                                 className="border border-solid inline-block border-gray-700 px-3 py-2 cursor-pointer"
@@ -113,7 +113,7 @@ export const Profile = () => {
                                 type="file"
                                 onChange={handleUpload}
                               />
-                            </div>
+                            </div> */}
                           </div>
 
                           <div className="col-span-6 sm:col-span-3">
@@ -130,84 +130,18 @@ export const Profile = () => {
                               className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                             >
                               <option>Sweden</option>
-                              <option>Stockholm</option>
-                              <option>Mexico</option>
                             </select>
-                          </div>
-
-                          <div className="col-span-6">
-                            <label
-                              htmlFor="street-address"
-                              className="block text-sm font-medium text-gray-700"
-                            >
-                              Street address
-                            </label>
-                            <input
-                              type="text"
-                              name="street-address"
-                              id="street-address"
-                              autoComplete="street-address"
-                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            />
-                          </div>
-
-                          <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                            <label
-                              htmlFor="city"
-                              className="block text-sm font-medium text-gray-700"
-                            >
-                              City
-                            </label>
-                            <input
-                              type="text"
-                              name="city"
-                              id="city"
-                              autoComplete="address-level2"
-                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            />
-                          </div>
-
-                          <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <label
-                              htmlFor="region"
-                              className="block text-sm font-medium text-gray-700"
-                            >
-                              State / Province
-                            </label>
-                            <input
-                              type="text"
-                              name="region"
-                              id="region"
-                              autoComplete="address-level1"
-                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            />
-                          </div>
-
-                          <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <label
-                              htmlFor="postal-code"
-                              className="block text-sm font-medium text-gray-700"
-                            >
-                              ZIP / Postal code
-                            </label>
-                            <input
-                              type="text"
-                              name="postal-code"
-                              id="postal-code"
-                              autoComplete="postal-code"
-                              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                            />
                           </div>
                         </div>
                       </div>
-                      <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                      {/* <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                         <button
                           type="submit"
                           className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
                           Save
                         </button>
-                      </div>
+                      </div> */}
                     </div>
                   </form>
                 </div>
