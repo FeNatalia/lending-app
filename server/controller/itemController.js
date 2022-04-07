96da647a998d7e29b3bbf421cc91d86bdcd49c07
Dovlat <dovlat93@gmail.com>
2022-04-07 17:07:49 +0200

    fix useEffect bug and sort items by createdAt date in desc

--- server/controller/itemController.js
+++ server/controller/itemController.js
@@ -1,18 +1,18 @@
-const Item = require("../model/item");
-const { notFound } = require("./errorHandler");
+const Item = require('../model/item');
+const { notFound } = require('./errorHandler');
 
 const addItem = (req, res) => {
   const item = new Item(req.body);
   item
     .save()
-    .then((result) => res.status(201).json(result))
-    .catch((err) => res.json({ error: err.message }));
+    .then(result => res.status(201).json(result))
+    .catch(err => res.json({ error: err.message }));
 };
 
 const getAllItems = (req, res) => {
   let query = {};
   if (req.query.q) {
-    query = { ...query, name: { $regex: req.query.q, $options: "i" } };
+    query = { ...query, name: { $regex: req.query.q, $options: 'i' } };
   }
   if (req.query.city) {
     query.city = req.query.city;
@@ -20,20 +20,22 @@ const getAllItems = (req, res) => {
   if (req.query.category) {
     query.category = req.query.category;
   }
-  Item.find(query).then((result) => {
-    return res.json(result);
-  });
+  Item.find(query)
+    .sort({ createdAt: -1 })
+    .then(result => {
+      return res.json(result);
+    });
 };
 
 const getItem = (req, res, next) => {
   Item.findById(req.params.id)
-    .then((result) => {
+    .then(result => {
       if (result === null) {
-        throw new Error("No items found with the provided id");
+        throw new Error('No items found with the provided id');
       }
       res.json(result);
     })
-    .catch((err) => notFound(err, next));
+    .catch(err => notFound(err, next));
 };
 
 module.exports = {
