d1a8d32595f1dcc81bde902f00e7bb05a4982f85
Dovlat <dovlat93@gmail.com>
2022-04-09 10:38:24 +0200

    fix firebase env vars

--- client/src/auth/firebase.js
+++ client/src/auth/firebase.js
@@ -2,23 +2,23 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfiguration = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BCKT,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
 // const firebaseConfiguration = {
-//   apiKey: 'AIzaSyAcX7KodyU8PH4bEDlUDBiRKQ4nPlh3WZ4',
-//   authDomain: 'auth-dev-81132.firebaseapp.com',
-//   projectId: 'auth-dev-81132',
-//   storageBucket: 'auth-dev-81132.appspot.com',
-//   messagingSenderId: '190164900032',
-//   appId: '1:190164900032:web:7f9f75c5306653937d3525',
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BCKT,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 // };
+
+const firebaseConfiguration = {
+  apiKey: 'AIzaSyAcX7KodyU8PH4bEDlUDBiRKQ4nPlh3WZ4',
+  authDomain: 'auth-dev-81132.firebaseapp.com',
+  projectId: 'auth-dev-81132',
+  storageBucket: 'auth-dev-81132.appspot.com',
+  messagingSenderId: '190164900032',
+  appId: '1:190164900032:web:7f9f75c5306653937d3525',
+};
 
 const firebaseInstance = initializeApp(firebaseConfiguration);
 
